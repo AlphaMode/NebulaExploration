aea1f2ae31c18dc568eccd4cc4da0d1027387468
AlphaMode <AlphaModeYT@gmail.com>
2021-05-07 20:54:05 -0500

    Remove remapped srv

--- remappedSrc/mojang/nebula/storage/OxygenTank.java
+++
@@ -1,8 +0,0 @@
-package alphamode.core.nebula.storage;
-
-public interface OxygenTank extends Tank {
-    /**
-     * @return Returns the current supply of oxygen in the tank
-     */
-    int getAmmount();
-}
