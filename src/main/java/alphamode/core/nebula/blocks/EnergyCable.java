74f85f22b5bc91c2f370200bf3b3e52dd2126358
AlphaMode <AlphaModeYT@gmail.com>
2021-05-28 11:51:23 -0500

    Update to loom 0.8

--- src/main/java/alphamode/core/nebula/blocks/EnergyCable.java
+++ src/main/java/alphamode/core/nebula/blocks/EnergyCable.java
@@ -1,6 +1,31 @@
 package alphamode.core.nebula.blocks;
 
 import alphamode.core.nebula.api.blocks.ICable;
+import java.util.Map;
 
-public class EnergyCable implements ICable<Energy> {
+public class EnergyCable implements ICable<Object> {
+    @Override
+    public Map<String, Object> get() {
+        return null;
+    }
+
+    @Override
+    public void add(Object value) {
+
+    }
+
+    @Override
+    public void add(String id, Object value) {
+
+    }
+
+    @Override
+    public void setParent(ICable parent) {
+
+    }
+
+    @Override
+    public boolean isParent() {
+        return false;
+    }
 }
